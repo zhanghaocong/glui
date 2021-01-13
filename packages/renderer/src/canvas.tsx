d1eab8bd34e9cc43df75e7497c383055621851ab
Zhang Haocong <kakera@gmail.com>
2021-01-14 00:16:38 +0800

    fixup

--- packages/renderer/src/canvas.tsx
+++ packages/renderer/src/canvas.tsx
@@ -1,9 +1,20 @@
 import { FC, memo } from 'react'
+import useMeasure from 'react-use-measure'
+
+const defaultStyles: React.CSSProperties = {
+  position: 'relative',
+  width: '100%',
+  height: '100%',
+  overflow: 'hidden',
+  backgroundColor: 'aqua',
+}
 
 export const Canvas: FC = memo(() => {
+  const [setRef, size, forceResize] = useMeasure({ scroll: true, debounce: { scroll: 50, resize: 0 }})
+  console.info(size)
   return (
-    <div>
-      <canvas style={{ display: 'block' }} />
+    <div ref={ setRef } style={ { ...defaultStyles } }>
+      <canvas width={size.width} height={size.height} style={{ display: 'block' }} />
     </div>
   )
 })
